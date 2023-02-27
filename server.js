63c2fdb0203a1c9a782fe797333ca61f445b7ea7
Dionne Angeles <jr8dionne@yahoo.com>
2023-02-26 17:16:03 -0800

    updated server.js

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social-network-api', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false // add this line to disable the use of `findAndModify`
+  useUnifiedTopology: true
 });
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false // add this line to disable the use of `findAndModify`
+// });
 
 
 mongoose.set('debug', true);
